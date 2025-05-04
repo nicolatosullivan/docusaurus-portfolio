b3ec58e82c4360a36c93767fd366660bf9779a0a
Nicola O'Sullivan <nicolaots@gmail.com>
2025-05-04 12:33:12 +0100

    Adding docusaurus files to GitHub

---
+++ src/pages/my-react-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>My Doc Portfolio</h1>
+      <p>This site contains a collection of samples showcasing various technical writing skills.</p>
+    </Layout>
+  );
+}
